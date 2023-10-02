f036fd9a22290dfe7a0afc8d3cd6c0543b626bcb
freaklucas <lucas.oliveira.freak@gmail.com>
2023-10-01 21:01:23 -0300

    test: implement update, remove

--- CrudQualidade.Tests/PeopleServiceTest.cs
+++ CrudQualidade.Tests/PeopleServiceTest.cs
@@ -43,7 +43,40 @@ namespace CrudQualidade.Tests
             
             Assert.Equal(expectedPeople.Name, result.Name);
         }
-        
+
+        [Fact]
+        public void UpdatePeople_ShouldUpdatePeople()
+        {
+            var mockPeople = new People { Id = 1, Name = "Lucas Editado" };
+            var mockRepo = new Mock<IPeopleRepository>();
+            mockRepo.Setup(repo => repo.UpdatePeople(mockPeople));
+
+            var mockUow = new Mock<IUnitOfWork>();
+            mockUow.Setup(uow => uow.PeopleRepository).Returns(mockRepo.Object);
+            var service = new PeopleService(mockUow.Object);
+            
+            service.UpdatePeople(mockPeople);
+            mockRepo.Verify(repo => repo.UpdatePeople(mockPeople), Times.Once());
+        }
+
+        [Fact]
+        public void RemovePeople_ShouldRemovePeople()
+        {
+            // Arrange -> requisitos
+            var mockPeople = new People{ Id = 1, Name = "Lucas"};
+            var mockRepo = new Mock<IPeopleRepository>();
+            mockRepo.Setup(repo => repo.DeletePeople(mockPeople));
+
+            var mockUow = new Mock<IUnitOfWork>();
+            mockUow.Setup(uow => uow.PeopleRepository).Returns(mockRepo.Object);
+
+            var service = new PeopleService(mockUow.Object);
+            // act -> funcionalidade
+            service.DeletePeople(mockPeople);
+            
+            //Asset -> esperado
+            mockRepo.Verify(repo => repo.DeletePeople(mockPeople), Times.Once());
+        }
         private List<People> GetTestPeoples()
         {
             return new List<People>
